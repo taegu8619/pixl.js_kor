6bfa6fcf6db055f6cf88688b8e946085cde00893
taegu8619 <ktg8619@naver.com>
2025-07-02 11:37:17 +0900

    한국어 번역 파일 추가 및 빌드 설정 수정

--- fw/application/src/i18n/language.c
+++ fw/application/src/i18n/language.c
@@ -1,4 +1,3 @@
-
 #include "language.h"
 
 typedef struct {
@@ -6,20 +5,14 @@ typedef struct {
 } LanguageData;
 
 const LanguageData const languageData[LANGUAGE_COUNT] = {
-    [LANGUAGE_ZH_HANS] = {.strings = lang_zh_Hans},
-    [LANGUAGE_EN_US] = {.strings = lang_en_US},
-    [LANGUAGE_ZH_TW] = {.strings = lang_zh_TW},
-    [LANGUAGE_ES_ES] = {.strings = lang_es_ES},
-    [LANGUAGE_HU_HU] = {.strings = lang_hu_HU},
-    [LANGUAGE_DE_DE] = {.strings = lang_de_DE},
-    [LANGUAGE_FR_FR] = {.strings = lang_fr_FR},
-    [LANGUAGE_NL_NL] = {.strings = lang_nl_NL},
-    [LANGUAGE_PT_BR] = {.strings = lang_pt_BR},
-    [LANGUAGE_JA_JP] = {.strings = lang_ja_JP},
-    [LANGUAGE_PT_PT] = {.strings = lang_pt_PT},
-    [LANGUAGE_IT_IT] = {.strings = lang_it_IT},
-    [LANGUAGE_RU_RU] = {.strings = lang_ru_RU},
-    [LANGUAGE_PL_PL] = {.strings = lang_pl_PL},
+    [LANGUAGE_ZH_HANS] = {.strings = lang_zh_Hans}, [LANGUAGE_EN_US] = {.strings = lang_en_US},
+    [LANGUAGE_ZH_TW] = {.strings = lang_zh_TW},     [LANGUAGE_ES_ES] = {.strings = lang_es_ES},
+    [LANGUAGE_HU_HU] = {.strings = lang_hu_HU},     [LANGUAGE_DE_DE] = {.strings = lang_de_DE},
+    [LANGUAGE_FR_FR] = {.strings = lang_fr_FR},     [LANGUAGE_NL_NL] = {.strings = lang_nl_NL},
+    [LANGUAGE_PT_BR] = {.strings = lang_pt_BR},     [LANGUAGE_JA_JP] = {.strings = lang_ja_JP},
+    [LANGUAGE_PT_PT] = {.strings = lang_pt_PT},     [LANGUAGE_IT_IT] = {.strings = lang_it_IT},
+    [LANGUAGE_RU_RU] = {.strings = lang_ru_RU},     [LANGUAGE_PL_PL] = {.strings = lang_pl_PL},
+    [LANGUAGE_KO_KR] = {.strings = lang_ko_KR}, // 한국어 데이터 추가
 };
 
 // 当前语言设置 (Current language setting)
@@ -40,36 +33,38 @@ void setLanguage(Language lang) { currentLanguage = lang; }
 
 const char *getLangDesc(Language lang) {
     switch (lang) {
-        case LANGUAGE_ZH_HANS:
-            return "简体中文";
-        case LANGUAGE_EN_US:
-            return "English";
-        case LANGUAGE_ZH_TW:
-            return "繁體中文(臺灣)";
-        case LANGUAGE_ES_ES:
-            return "Español";
-        case LANGUAGE_HU_HU:
-            return "Magyar";
-        case LANGUAGE_DE_DE:
-            return "Deutsch";
-        case LANGUAGE_FR_FR:
-            return "Français";
-        case LANGUAGE_NL_NL:
-            return "Dutch (Nederlands)";
-        case LANGUAGE_PT_BR:
-            return "Português (Brazil)";
-        case LANGUAGE_JA_JP:
-            return "日本語";
-        case LANGUAGE_PT_PT:
-            return "Português (Portugal)";
-        case LANGUAGE_IT_IT:
-            return "Italiano";
-        case LANGUAGE_RU_RU:
-            return "Русский";
-        case LANGUAGE_PL_PL:
-            return "Polski";
-        default:
-            return "@@LANG@@";
+    case LANGUAGE_ZH_HANS:
+        return "简体中文";
+    case LANGUAGE_EN_US:
+        return "English";
+    case LANGUAGE_ZH_TW:
+        return "繁體中文(臺灣)";
+    case LANGUAGE_ES_ES:
+        return "Español";
+    case LANGUAGE_HU_HU:
+        return "Magyar";
+    case LANGUAGE_DE_DE:
+        return "Deutsch";
+    case LANGUAGE_FR_FR:
+        return "Français";
+    case LANGUAGE_NL_NL:
+        return "Dutch (Nederlands)";
+    case LANGUAGE_PT_BR:
+        return "Português (Brazil)";
+    case LANGUAGE_JA_JP:
+        return "日本語";
+    case LANGUAGE_PT_PT:
+        return "Português (Portugal)";
+    case LANGUAGE_IT_IT:
+        return "Italiano";
+    case LANGUAGE_RU_RU:
+        return "Русский";
+    case LANGUAGE_PL_PL:
+        return "Polski";
+    case LANGUAGE_KO_KR:
+        return "한국어"; // 한국어 설명 추가
+    default:
+        return "@@LANG@@";
     }
 }
 
--- fw/application/src/i18n/language.h
+++ fw/application/src/i18n/language.h
@@ -9,7 +9,6 @@
 
 #define _T(x) getLangString(_L_##x)
 
-
 typedef enum {
     LANGUAGE_ZH_HANS,
     LANGUAGE_EN_US,
@@ -25,29 +24,30 @@ typedef enum {
     LANGUAGE_IT_IT,
     LANGUAGE_RU_RU,
     LANGUAGE_PL_PL,
+    LANGUAGE_KO_KR, // 한국어 추가
     LANGUAGE_COUNT
 } Language;
 
-extern const char* lang_en_US[_L_COUNT];
-extern const char* lang_zh_Hans[_L_COUNT];
-extern const char* lang_zh_TW[_L_COUNT];
-extern const char* lang_es_ES[_L_COUNT];
-extern const char* lang_hu_HU[_L_COUNT];
-extern const char* lang_de_DE[_L_COUNT];
-extern const char* lang_fr_FR[_L_COUNT];
-extern const char* lang_nl_NL[_L_COUNT];
-extern const char* lang_pt_BR[_L_COUNT];
-extern const char* lang_ja_JP[_L_COUNT];
-extern const char* lang_pt_PT[_L_COUNT];
-extern const char* lang_it_IT[_L_COUNT];
-extern const char* lang_ru_RU[_L_COUNT];
-extern const char* lang_pl_PL[_L_COUNT];
+extern const char *lang_en_US[_L_COUNT];
+extern const char *lang_zh_Hans[_L_COUNT];
+extern const char *lang_zh_TW[_L_COUNT];
+extern const char *lang_es_ES[_L_COUNT];
+extern const char *lang_hu_HU[_L_COUNT];
+extern const char *lang_de_DE[_L_COUNT];
+extern const char *lang_fr_FR[_L_COUNT];
+extern const char *lang_nl_NL[_L_COUNT];
+extern const char *lang_pt_BR[_L_COUNT];
+extern const char *lang_ja_JP[_L_COUNT];
+extern const char *lang_pt_PT[_L_COUNT];
+extern const char *lang_it_IT[_L_COUNT];
+extern const char *lang_ru_RU[_L_COUNT];
+extern const char *lang_pl_PL[_L_COUNT];
+extern const char *lang_ko_KR[_L_COUNT]; // 한국어 문자열 배열 선언 추가
 
 // 获取字符串的函数 (Get language string function)
-const char* getLangString(L_StringID stringID);
+const char *getLangString(L_StringID stringID);
 void setLanguage(Language lang);
 Language getLanguage();
-const char* getLangDesc(Language lang);
-
+const char *getLangDesc(Language lang);
 
 #endif // LANGUAGE_H
