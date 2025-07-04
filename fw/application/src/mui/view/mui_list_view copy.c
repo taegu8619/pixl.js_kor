ed421baeac8a740da079ad7995455a81115cec86
taegu8619 <ktg8619@naver.com>
2025-07-04 22:21:35 +0900

    Add Korean translation files and update build settings

--- fw/application/src/mui/view/mui_list_view copy.c
+++ fw/application/src/mui/view/mui_list_view copy.c
@@ -14,30 +14,16 @@ static uint16_t mui_list_view_get_utf8_width(const char *str) { return u8g2_GetU
 static void mui_list_view_start_text_anim(mui_list_view_t *p_view) {
     if (mui_list_view_anim_enabled()) {
         mui_list_item_t *p_item = mui_list_item_array_get(p_view->items, p_view->focus_index);
-        // p_item이 NULL이 아닌지 확인
-        if (!p_item) {
-            mui_anim_stop(&p_view->text_anim);
-            return;
-        }
         uint32_t focus_text_width = mui_list_view_get_utf8_width(string_get_cstr(p_item->text));
-
-        // 부가 텍스트 너비는 따로 계산 (합치지 않음)
-        // focus_text_width += mui_list_view_get_utf8_width(string_get_cstr(p_item->sub_text));
-
-        // 화면 너비보다 텍스트가 길 때만 애니메이션을 실행합니다.
-        if (focus_text_width > (p_view->canvas_width - 13)) {
+        focus_text_width += mui_list_view_get_utf8_width(string_get_cstr(p_item->sub_text));
+        if (focus_text_width > p_view->canvas_width - 13) {
             p_view->text_offset = 0;
-            // 스크롤 될 전체 너비 (텍스트 길이 + 반복을 위한 간격)
-            int32_t total_scroll_width = focus_text_width + 30; // 30은 텍스트 반복 시 간격
-
-            // 애니메이션 시간과 값 설정
-            mui_anim_set_time(&p_view->text_anim, total_scroll_width * 50); // 속도 조절 (값이 클수록 느려짐)
-            mui_anim_set_values(&p_view->text_anim, 0, -total_scroll_width);
-            mui_anim_set_auto_restart(&p_view->text_anim, true); // 애니메이션 자동 반복
-            mui_anim_set_start_delay(&p_view->text_anim, 1000);  // 1초 후 시작
+            int32_t overflowed_width = focus_text_width - p_view->canvas_width + 20;
+            mui_anim_set_time(&p_view->text_anim, overflowed_width * 100);
+            mui_anim_set_values(&p_view->text_anim, 0, -overflowed_width);
+            mui_anim_set_auto_restart(&p_view->text_anim, true);
             mui_anim_start(&p_view->text_anim);
         } else {
-            // 텍스트가 짧으면 애니메이션을 멈추고 오프셋을 초기화합니다.
             p_view->text_offset = 0;
             mui_anim_stop(&p_view->text_anim);
         }
@@ -81,75 +67,54 @@ static void mui_list_view_on_draw(mui_view_t *p_view, mui_canvas_t *p_canvas) {
         offset_y += p_mui_list_view->anim_value;
     }
     uint32_t index = 0;
-    // uint32_t focus_y = 0; // 사용되지 않는 변수
+    uint32_t focus_y = 0;
     mui_rect_t clip_win_prev;
     mui_rect_t clip_win_cur;
     while (!mui_list_item_array_end_p(it)) {
         mui_list_item_t *item = mui_list_item_array_ref(it);
         int32_t y = index * LIST_ITEM_HEIGHT - offset_y;
-
+        int32_t text_offset = index == p_mui_list_view->focus_index ? p_mui_list_view->text_offset : 0;
         if (y >= -LIST_ITEM_HEIGHT && y <= mui_canvas_get_height(p_canvas)) { // visible object
             uint32_t actual_y = index * p_mui_list_view->item_gap - offset_y;
-
-            // 아이콘 그리기
             mui_canvas_set_font(p_canvas, u8g2_font_siji_t_6x10);
             mui_canvas_draw_glyph(p_canvas, 0, actual_y + 10, item->icon);
             mui_canvas_set_font(p_canvas, u8g2_font_wqy12_t_gb2312a);
+            mui_canvas_get_clip_window(p_canvas, &clip_win_prev);
+            clip_win_cur.x = 13;
+            clip_win_cur.y = actual_y;
+            clip_win_cur.h = LIST_ITEM_HEIGHT, clip_win_cur.w = mui_canvas_get_width(p_canvas);
+            mui_canvas_set_clip_window(p_canvas, &clip_win_cur);
 
-            // --- 포커스된 항목 그리기 로직 수정 ---
-            if (index == p_mui_list_view->focus_index) {
-                // 선택 상자 그리기
-                mui_canvas_set_draw_color(p_canvas, 2); // 반전 색상
-                mui_canvas_draw_box(p_canvas, 0, actual_y, mui_canvas_get_width(p_canvas), LIST_ITEM_HEIGHT - 1);
-                mui_canvas_set_draw_color(p_canvas, 1); // 원래 색상
-
-                // 클리핑 영역 설정 (아이콘 영역 제외)
-                mui_canvas_get_clip_window(p_canvas, &clip_win_prev);
-                clip_win_cur.x = 13;
-                clip_win_cur.y = actual_y;
-                clip_win_cur.h = LIST_ITEM_HEIGHT;
-                clip_win_cur.w = mui_canvas_get_width(p_canvas) - 13;
-
-                // 부가 텍스트가 있으면 너비 계산에서 제외
-                if (string_size(item->sub_text) > 0) {
-                    uint8_t sub_text_width = mui_canvas_get_utf8_width(p_canvas, string_get_cstr(item->sub_text));
-                    clip_win_cur.w -= (sub_text_width + 5); // 여백 포함
-                }
-                mui_canvas_set_clip_window(p_canvas, &clip_win_cur);
-
-                uint32_t text_width = mui_list_view_get_utf8_width(string_get_cstr(item->text));
-
-                // 마퀴 효과 적용
-                if (text_width > clip_win_cur.w) {
-                    const int gap_pixels = 30;
-                    mui_canvas_draw_utf8(p_canvas, 13 + p_mui_list_view->text_offset, actual_y + 10,
-                                         string_get_cstr(item->text));
-                    mui_canvas_draw_utf8(p_canvas, 13 + p_mui_list_view->text_offset + text_width + gap_pixels,
-                                         actual_y + 10, string_get_cstr(item->text));
-                } else {
-                    mui_canvas_draw_utf8(p_canvas, 13, actual_y + 10, string_get_cstr(item->text));
-                }
-
-                // 클리핑 영역 복원
-                mui_canvas_set_clip_window(p_canvas, &clip_win_prev);
-            } else {
-                // 선택되지 않은 항목 그리기
-                mui_canvas_draw_utf8_clip(p_canvas, 13, actual_y + 10, string_get_cstr(item->text));
-            }
-
-            // 부가 텍스트 그리기 (기존 로직 유지)
+            uint32_t focus_text_width =
+                mui_canvas_draw_utf8_clip(p_canvas, 13 + text_offset, actual_y + 10, string_get_cstr(item->text));
+            // sub text
             if (string_size(item->sub_text) > 0) {
                 uint8_t w = mui_canvas_get_utf8_width(p_canvas, string_get_cstr(item->sub_text));
-                mui_canvas_draw_utf8(p_canvas, mui_canvas_get_width(p_canvas) - w - 5, actual_y + 10,
-                                     string_get_cstr(item->sub_text));
+                if (focus_text_width + w > p_mui_list_view->canvas_width - 13) {
+                    mui_canvas_draw_utf8_clip(p_canvas, 13 + text_offset + focus_text_width, actual_y + 10,
+                                              string_get_cstr(item->sub_text));
+                } else {
+                    mui_canvas_draw_utf8(p_canvas, mui_canvas_get_width(p_canvas) - w - 5, actual_y + 10,
+                                         string_get_cstr(item->sub_text));
+                }
             }
+
+            mui_canvas_set_clip_window(p_canvas, &clip_win_prev);
         }
 
         mui_list_item_array_next(it);
         index++;
     }
+    int32_t focus_yi = p_mui_list_view->focus_index * LIST_ITEM_HEIGHT - offset_y;
+    if (p_mui_list_view->anim_type == LIST_ANIM_FOCUS) {
+        focus_yi = focus_yi + p_mui_list_view->anim_value;
+    }
+    focus_yi = LV_MIN(LV_MAX(focus_yi, 0), mui_canvas_get_height(p_canvas) - LIST_ITEM_HEIGHT);
+    uint32_t focus_h = 12;
+    mui_canvas_set_draw_color(p_canvas, 2);
+    mui_canvas_draw_box(p_canvas, 0, focus_yi, mui_canvas_get_width(p_canvas), focus_h);
+    mui_canvas_set_draw_color(p_canvas, 1);
 
-    // 스크롤바 그리기
     mui_element_scrollbar(p_canvas, p_mui_list_view->focus_index, mui_list_item_array_size(p_mui_list_view->items));
 
     if (!p_mui_list_view->first_draw) {
@@ -193,8 +158,7 @@ static void mui_list_view_on_input(mui_view_t *p_view, mui_input_event_t *event)
                 if (focus_offset >
                     p_mui_list_view->scroll_offset + p_mui_list_view->canvas_height) { // scroll to bottom
                     uint32_t cur_scroll_offset = p_mui_list_view->scroll_offset;
-                    p_mui_list_view->scroll_offset =
-                        (p_mui_list_view->focus_index - max_item_num + 1) * LIST_ITEM_HEIGHT;
+                    p_mui_list_view->scroll_offset = (p_mui_list_view->focus_index - max_item_num) * LIST_ITEM_HEIGHT;
                     uint32_t diff_scroll_offset = p_mui_list_view->scroll_offset - cur_scroll_offset;
                     if (mui_list_view_anim_enabled()) {
                         p_mui_list_view->anim_type = LIST_ANIM_SCROLL;
@@ -221,9 +185,7 @@ static void mui_list_view_on_input(mui_view_t *p_view, mui_input_event_t *event)
             if (p_mui_list_view->focus_index < mui_list_item_array_size(p_mui_list_view->items) - 1) {
                 p_mui_list_view->focus_index++;
                 uint16_t focus_offset = p_mui_list_view->focus_index * LIST_ITEM_HEIGHT;
-                if (focus_offset >=
-                    p_mui_list_view->scroll_offset +
-                        (p_mui_list_view->canvas_height / LIST_ITEM_HEIGHT) * LIST_ITEM_HEIGHT) { // scroll down
+                if (focus_offset > p_mui_list_view->scroll_offset + p_mui_list_view->canvas_height) { // scroll down
                     p_mui_list_view->scroll_offset += LIST_ITEM_HEIGHT;
 
                     if (mui_list_view_anim_enabled()) {
@@ -233,6 +195,7 @@ static void mui_list_view_on_input(mui_view_t *p_view, mui_input_event_t *event)
                         mui_anim_set_values(&p_mui_list_view->anim, -LIST_ITEM_HEIGHT, 0);
                         mui_anim_start(&p_mui_list_view->anim);
                     }
+                    // todo scroll anim
                 } else {
                     if (mui_list_view_anim_enabled()) {
                         p_mui_list_view->anim_value = -LIST_ITEM_HEIGHT;
--- fw/scripts/amiibo_db_gen.py
+++ fw/scripts/amiibo_db_gen.py
@@ -6,95 +6,127 @@ import json
 import os
 import csv
 
-# --- 전체 한글 번역 데이터 시작 ---
+--- 한글 번역 데이터 (전체) 시작 ---
 amiibo_translation = {
-    # Animal Crossing
-    "Sandy": "샌디", "Isabelle - Winter": "여울 (겨울옷)", "Ava": "에바", "Blanca": "블랑카", "Mac": "맥", "Lucha": "루차", "Punchy": "빙티",
-    "Violet": "비올라", "Tom": "톰", "Mint": "민트", "Caroline": "캐롤라인", "Mabel": "고순이", "Frett": "챔프", "Kidd": "염두리",
-    "Purrl": "타마", "Mitzi": "마르", "Reneigh": "리아나", "Rasher": "글레이", "Chrissy": "크리스틴", "Harriet": "카트리나", "Daisy": "데이지",
-    "Bam": "록키", "Anabelle": "아롱이", "Labelle": "라벨", "Rover": "낯선고양이", "Wendell": "세이", "Renée": "르네", "Agnes": "아그네스",
-    "Resetti - Without Hat": "도루묵씨 (모자 없음)", "Daisy Mae": "무파니", "Merry": "유네찌", "Big Top": "1호", "Leif": "늘봉", "Rocco": "아폴로",
-    "Gladys": "글라라", "Twiggy": "핀틱", "Camofrog": "충성", "Lottie": "솜이", "Deirdre": "나디아", "Flick": "레온", "Naomi": "화자",
+    # 슈퍼 마리오 시리즈
+    "Mario": "마리오", "Luigi": "루이지", "Peach": "피치공주", "Bowser": "쿠파", "Dr. Mario": "닥터 마리오",
+    "Rosalina": "로젤리나", "Bowser Jr.": "쿠파주니어", "Wario": "와리오", "Yoshi": "요시", "Donkey Kong": "동키콩",
+    "Diddy Kong": "디디콩", "Daisy": "데이지", "Waluigi": "와루이지", "Boo": "부끄부끄", "Goomba": "굼바",
+    "Koopa Troopa": "엉금엉금", "Wedding Mario": "마리오 (웨딩스타일)", "Wedding Peach": "피치 (웨딩스타일)", "Wedding Bowser": "쿠파 (웨딩스타일)",
+    "Mario - Cat": "고양이마리오", "Peach - Cat": "고양이피치", "Mario - Gold Edition": "마리오 (골드 버전)",
+    "Mario - Silver Edition": "마리오 (실버 버전)", "8-Bit Mario Modern Color": "8비트 마리오 (모던 컬러)",
+    "8-Bit Mario Classic Color": "8비트 마리오 (클래식 컬러)", "Mario - Power Up Band": "마리오 (파워업 밴드)",
+    "Luigi - Power Up Band": "루이지 (파워업 밴드)", "Peach - Power Up Band": "피치 (파워업 밴드)",
+    "Daisy - Power Up Band": "데이지 (파워업 밴드)", "Toad - Power Up Band": "키노피오 (파워업 밴드)",
+    "Yoshi - Power Up Band": "요시 (파워업 밴드)", "Golden - Power Up Band": "골든 (파워업 밴드)",
+    "Donkey Kong - Power Up Band": "동키콩 (파워업 밴드)", "Diddy Kong - Power Up Band": "디디콩 (파워업 밴드)",
 
-    "Raddle": "개군", "Tortimer": "고북", "Digby": "켄트", "Puddles": "가위", "Pashmina": "바바라", "Paolo": "파올로", "June": "쭈니",
-    "Rudy": "루돌", "Flurry": "뽀야미", "Tammy": "아네사", "Teddy": "곰도로스", "Frank": "헐크", "Stu": "모리스", "Roscoe": "로데오",
-    "Rodney": "지미", "Bruce": "브루스", "Bree": "사라", "Bangle": "루주", "Stitches": "패치", "Aurora": "오로라", "Iggly": "펭수",
-    "Vic": "빅터", "Amelia": "안데스", "Miranda": "미랑", "Katrina": "마추릴라", "Audie": "모니카", "Bill": "코코아", "Anchovy": "앤쵸비",
-    "Harvey": "파니엘", "Tommy - Uniform": "밤돌이 (유니폼)", "Maggie": "마가렛", "Orville": "로드리", "Chai": "차이", "Dom": "차둘",
-    "Kapp'n": "갑돌", "Limberg": "단무지", "Weber": "아잠만", "Bunnie": "릴리안", "Pelly": "펠리", "Frobert": "구리구리", "Tasha": "나타샤",
-    "Robin": "로빈", "Alfonso": "알베르트", "Peck": "쪼끼", "Cyrus": "리포", "Frita": "웬디", "Sprinkle": "크리미", "Bella": "이자벨",
-    "Drago": "용남이", "Grams": "갑순", "OHare": "오골", "Hamphrey": "햄둥", "Pave": "베르리나", "Alli": "크로크", "Bones": "토미",
-    "Gaston": "대길", "Quillson": "덕", "Gwen": "폴라", "Kevin": "멧지", "Blaire": "실루엣", "Wolfgang": "시베리아", "Peggy": "꽃지",
-    "Boyd": "덤벨", "Gabi": "패티카", "T-Bone": "티본", "Portia": "블랜더", "Benedict": "페니실린", "Pudge": "우띠", "Jay": "참돌이",
-    "Tabby": "호랭이", "Kody": "아이루", "Apollo": "아폴로", "Maple": "메이첼", "Sly": "하이드", "Goose": "건태", "Cookie": "베리",
-
-    "Pietro": "피에로", "Henry": "헨리", "Blathers": "부엉", "Timbra": "잔디", "Ed": "꺼벙", "Sable": "고옥이", "Keaton": "프랭크",
-    "Hugh": "먹고파", "Wart Jr.": "샘", "Kicks": "패트릭", "Shrunk": "스승", "Eugene": "코알", "Pecan": "리키", "Sparro": "춘섭",
-    "Isabelle - Dress": "여울 (원피스)", "Kabuki": "가북희", "Eloise": "엘레핀", "Gala": "꽃돼지", "Tex": "펭기", "Reese": "리사",
-    "Bianca": "백희", "Cephalobot": "기계로", "Fuchsia": "제시카", "Tucker": "맘모", "Annalisa": "설백", "Yuka": "유카리", "Pate": "나키",
-    "Carmen": "초코", "Lily": "레이니", "Bob": "히죽", "Ankha": "클레오", "Zucker": "쭈니", "Jack": "펌킹", "Sylvia": "실비아",
-    "Whitney": "비앙카", "Redd - Shirt": "여욱 (셔츠)", "Chevre": "윤이", "Shep": "존", "Fang": "시베리아", "Snake": "닌토",
-    "Pekoe": "재스민", "Norma": "젖소", "Butch": "존", "Marshal": "쭈니", "Cleo": "클레오", "Hornsby": "뿌람", "Dobie": "켄",
-    "Tiansheng": "손오공", "Ace": "에이스", "Zoe": "팽기", "Chabwick": "펭구", "Ione": "스피카", "Marlo": "돈후앙", "Petri": "리카",
-    "Quinn": "샹펜", "Rio": "리카", "Sasha": "미첼", "Shino": "요비", "Azalea": "鹃", "Faith": "믿음", "Frett": "챔프",
-    "Reneigh": "리아나", "Cephalobot": "기계로", "Ione": "스피카", "Marlo": "돈후앙", "Petri": "리카", "Quinn": "샹펜", "Rio": "리카",
-    "Sasha": "미첼", "Shino": "요비", "Tiansheng": "손오공", "Zoe": "팽기", "Ace": "에이스", "Azalea": "鹃", "Faith": "믿음",
-    "Frett": "챔프", "Reneigh": "리아나", "Cephalobot": "기계로", "Ione": "스피카", "Marlo": "돈후앙", "Petri": "리카", "Quinn": "샹펜",
-    "Rio": "리카", "Sasha": "미첼", "Shino": "요비", "Tiansheng": "손오공", "Zoe": "팽기", "Ace": "에이스", "Azalea": "鹃",
-    "Faith": "믿음", "Frett": "챔프", "Reneigh": "리아나", "Cephalobot": "기계로", "Ione": "스피카", "Marlo": "돈후앙",
-    "Petri": "리카", "Quinn": "샹펜", "Rio": "리카", "Sasha": "미첼", "Shino": "요비", "Tiansheng": "손오공", "Zoe": "팽기",
-    "Ace": "에이스", "Azalea": "鹃", "Faith": "믿음", "Frett": "챔프", "Reneigh": "리아나", "Cephalobot": "기계로",
-    "Ione": "스피카", "Marlo": "돈후앙", "Petri": "리카", "Quinn": "샹펜", "Rio": "리카", "Sasha": "미첼", "Shino": "요비",
-
-    # Super Mario
-    "Mario - Cat": "고양이마리오", "Peach - Cat": "고양이피치", "Mario - Gold Edition": "골드 마리오", "Mario - Silver Edition": "실버 마리오",
-    "Goomba": "굼바", "Koopa Troopa": "엉금엉금", "Boo": "부끄부끄", "Waluigi": "와루이지", "Diddy Kong": "디디콩",
-    "Rosalina": "로젤리나", "Bowser - Wedding": "웨딩 쿠파", "Peach - Wedding": "웨딩 피치", "Mario - Wedding": "웨딩 마리오",
-
-    # The Legend of Zelda
-    "Zelda & Loftwing": "젤다 & 로프트버드", "Link - Archer": "링크 (활)", "Link - Rider": "링크 (라이더)", "Guardian": "가디언",
-    "Bokoblin": "보코블린", "Daruk": "다르케르", "Mipha": "미파", "Revali": "리발", "Urbosa": "우르보사",
+    # 젤다의 전설 시리즈
+    "Link": "링크", "Zelda": "젤다", "Sheik": "시크", "Ganondorf": "가논돌프", "Toon Link": "툰링크",
+    "Wolf Link": "울프 링크", "Guardian": "가디언", "Bokoblin": "보코블린", "Zelda (BotW)": "젤다 (브레스 오브 더 와일드)",
+    "Link (Archer)": "링크 (활)", "Link (Rider)": "링크 (라이더)", "Daruk": "다르케르", "Mipha": "미파",
+    "Revali": "리발", "Urbosa": "우르보사", "Link (Majora's Mask)": "링크 (무쥬라의 가면)",
+    "Link (Skyward Sword)": "링크 (스카이워드 소드)", "Link (Twilight Princess)": "링크 (황혼의 공주)",
+    "8-Bit Link": "8비트 링크", "Link (Ocarina of Time)": "링크 (시간의 오카리나)", "Zelda & Loftwing": "젤다 & 로프트버드",
     "Toon Link - The Wind Waker": "툰링크 (바람의 지휘봉)", "Toon Zelda - The Wind Waker": "툰젤다 (바람의 지휘봉)",
-    "8-Bit Link": "8비트 링크", "Link - Ocarina of Time": "링크 (시간의 오카리나)", "Link - Majora's Mask": "링크 (무쥬라의 가면)",
-    "Link - Twilight Princess": "링크 (황혼의 공주)", "Wolf Link": "울프 링크", "Link - Skyward Sword": "링크 (스카이워드 소드)",
-    "Link - Link's Awakening": "링크 (꿈꾸는 섬)", "Link - Tears of the Kingdom": "링크 (티어스 오브 더 킹덤)",
-    "Zelda - Tears of the Kingdom": "젤다 (티어스 오브 더 킹덤)", "Ganondorf - Tears of the Kingdom": "가논돌프 (티어스 오브 더 킹덤)",
+    "Midna & Wolf Link": "미드나 & 울프 링크", "Link - Link's Awakening": "링크 (꿈꾸는 섬)",
+    "Link - Tears of the Kingdom": "링크 (티어스 오브 더 킹덤)", "Zelda - Tears of the Kingdom": "젤다 (티어스 오브 더 킹덤)",
+    "Ganondorf - Tears of the Kingdom": "가논돌프 (티어스 오브 더 킹덤)", "Tulin": "튤리", "Yunobo": "윤돌", "Sidon": "시드", "Riju": "루쥬",
 
-    # Super Smash Bros.
-    "Mii Brawler": "Mii 격투가", "Mii Swordfighter": "Mii 검술가", "Mii Gunner": "Mii 사격수", "Rosalina & Luma": "로젤리나 & 치코",
-    "King K. Rool": "킹크루루", "Lucina": "루키나", "Ike": "아이크", "Robin": "러플레", "Roy": "로이", "Corrin": "카무이",
-    "Corrin - Player 2": "카무이 (2P)", "Dark Pit": "블랙피트", "Zero Suit Samus": "제로 슈트 사무스", "Piranha Plant": "뻐끔플라워",
-    "Ice Climbers": "아이스 클라이머", "Incineroar": "어흥염", "Isabelle": "여울", "Joker": "조커", "Hero": "용사",
-    "Banjo & Kazooie": "반조 & 카주이", "Terry": "테리", "Byleth": "벨레트", "Min Min": "미엔미엔", "Steve": "스티브",
-    "Alex": "알렉스", "Sephiroth": "세피로스", "Pyra": "호무라", "Mythra": "히카리", "Kazuya": "카즈야", "Sora": "소라",
+    # 동물의 숲 시리즈
+    "Isabelle": "여울", "Tom Nook": "너굴", "K.K.": "T.K.", "Mabel": "고순이", "Reese": "리사", "Cyrus": "리포",
+    "Lottie": "솜이", "Celeste": "부옥", "Blathers": "부엉", "Kicks": "패트릭", "Rover": "낯선고양이",
+    "Timmy & Tommy": "콩돌이 & 밤돌이", "Digby": "켄트", "Resetti": "도루묵씨", "Kapp'n": "갑돌",
+    "Flick": "레온", "C.J.": "저스틴", "Daisy Mae": "무파니", "Orville": "로드리", "Wilbur": "모리",
+    "Harvey": "파니엘", "Wardell": "너티", "Niko": "니코", "Isabelle - Summer Outfit": "여울 (여름옷)",
+    "Isabelle - Winter Outfit": "여울 (겨울옷)", "DJ KK": "DJ T.K.", "Label": "라벨", "Labelle": "라벨",
+    "Sandy": "샌디", "Ava": "에바", "Blanca": "블랑카", "Mac": "맥", "Lucha": "루차", "Punchy": "빙티", "Violet": "바이올렛",
+    "Tom": "밴덤", "Mint": "민트", "Caroline": "캐롤라인", "Frett": "샹펜", "Kidd": "염두리", "Purrl": "타마", "Mitzi": "마르",
+    "Reneigh": "리아나", "Rasher": "그루민", "Chrissy": "크리스틴", "Harriet": "카트리나", "Bam": "록키", "Anabelle": "아롱이",
+    "Renée": "르네", "Agnes": "아그네스", "Resetti - Without Hat": "도루묵씨 (모자 없음)", "Merry": "유네찌", "Big Top": "코코아",
+    "Leif": "늘봉", "Rocco": "록코", "Gladys": "빅토리아", "Twiggy": "핀틱", "Camofrog": "충성", "Deirdre": "나디아",
+    "Naomi": "올리비아", "Raddle": "개군", "Tortimer": "고북", "Puddles": "가위", "Gaston": "산토스", "Quillson": "덕",
+    "Pashmina": "바바라", "Paolo": "파올로", "June": "쭈니", "Stitches": "패치", "Aurora": "오로라", "Iggly": "빙수",
+    "Vic": "노르망", "Amelia": "안데스", "Miranda": "미란다", "Katrina": "마추릴라", "Audie": "모니카", "Bill": "코코아",
+    "Anchovy": "안쵸비", "Tommy - Uniform": "콩돌이 (유니폼)", "Maggie": "마가렛", "Hopkins": "홉킨스", "Tasha": "나타샤",
+    "Robin": "파틱", "Alfonso": "알베르트", "Peck": "쪼아", "Rocket": "4호", "Peaches": "말자", "Ken": "오골",
+    "Stinky": "땀띠", "Francine": "프랑소와", "Roald": "펭수", "Monique": "제인", "Tybalt": "티볼트", "Melba": "아델레이드",
+    "Sylvia": "실비아", "Whitney": "비앙카", "Soleil": "샨티", "Sparro": "춘섭", "Spike": "스쿼트", "Sprinkle": "크리미",
+    "Sprocket": "헤르츠", "Static": "스파크", "Stella": "아크릴", "Sterling": "은수리", "Stitches - Amiibo Festival": "패치 (아미보 페스티벌)",
+    "Stu": "모리스", "Sydney": "시드니", "Sylvana": "실바나", "T-Bone": "티본", "Tabby": "호떡", "Tad": "텀보", "Tammi": "원숭",
+    "Tammy": "아네사", "Tangy": "귤", "Teddy": "곰비", "Tex": "펭기", "Tia": "티나", "Tiffany": "바네사", "Timbra": "잔디",
+    "Tipper": "마리", "Toby": "토비", "Truffles": "탱고", "Tucker": "맘모", "Tutu": "연유", "Twiggy": "핀틱", "Tybalt": "티볼트",
+    "Ursala": "네이아", "Velma": "벨마", "Vesta": "메리어스", "Vic": "노르망", "Victoria": "센트엘로", "Violet": "줌마",
 
-    # Splatoon
-    "Inkling - Yellow": "잉클링 (노랑)", "Shiver": "후우카", "Frye": "우츠호", "Big Man": "만타로", "Smallfry": "꼬마연어",
-    "Inkling Girl - Lime Green": "잉클링 걸 (라임 그린)", "Inkling Boy - Purple": "잉클링 보이 (보라)", "Inkling Squid - Orange": "잉클링 스퀴드 (주황)",
-    "Octoling - Blue": "옥토링 (파랑)",
+    # 스플래툰 시리즈
+    "Inkling Girl": "잉클링 걸", "Inkling Boy": "잉클링 보이", "Inkling Squid": "잉클링 스퀴드",
+    "Callie": "아오리", "Marie": "호타루", "Pearl": "히메", "Marina": "이이다", "Octoling Girl": "옥토링 걸",
+    "Octoling Boy": "옥토링 보이", "Octoling Octopus": "옥토링 옥토퍼스", "Smallfry": "꼬마연어",
+    "Inkling - Yellow": "잉클링 (옐로)", "Shiver": "후우카", "Frye": "우츠호", "Big Man": "만타로",
+    "Inkling Girl - Neon Pink": "잉클링 걸 (네온 핑크)", "Inkling Boy - Neon Green": "잉클링 보이 (네온 그린)",
+    "Inkling Squid - Neon Purple": "잉클링 스퀴드 (네온 퍼플)", "Octoling - Blue": "옥토링 (블루)",
+    "Inkling Girl - Lime Green": "잉클링 걸 (라임 그린)", "Inkling Boy - Purple": "잉클링 보이 (퍼플)",
+    "Inkling Squid - Orange": "잉클링 스퀴드 (오렌지)", "Callie - Alterna": "아오리 (얼터너)", "Marie - Alterna": "호타루 (얼터너)",
+    "Pearl - Side Order": "히메 (사이드 오더)", "Marina - Side Order": "이이다 (사이드 오더)",
 
-    # 기타
-    "Qbby": "큐비", "Detective Pikachu": "명탐정 피카츄", "Solaire of Astora": "태양의 전사 솔라", "Loot Goblin": "보물 고블린",
-    "Shovel Knight - Gold Edition": "골드 삽질 기사", "Plague Knight": "역병 기사", "Specter Knight": "스펙터 기사",
-    "King Knight": "킹 기사", "Mega Man - Gold Edition": "록맨 (골드 에디션)", "Super Mario Cereal": "슈퍼 마리오 시리얼"
+    # 파이어 엠블렘 시리즈
+    "Marth": "마르스", "Ike": "아이크", "Robin": "러플레", "Lucina": "루키나", "Roy": "로이", "Corrin": "카무이",
+    "Celica": "세리카", "Alm": "아름", "Tiki": "치키", "Chrom": "크롬", "Byleth": "벨레트",
+    "Corrin - Player 2": "카무이 (2P)",
+
+    # 커비 시리즈
+    "Kirby": "커비", "King Dedede": "디디디 대왕", "Meta Knight": "메타 나이트", "Waddle Dee": "웨이들 디",
+
+    # 포켓몬 시리즈
+    "Pikachu": "피카츄", "Charizard": "리자몽", "Lucario": "루카리오", "Jigglypuff": "푸린", "Greninja": "개굴닌자",
+    "Mewtwo": "뮤츠", "Squirtle": "꼬부기", "Ivysaur": "이상해풀", "Pokemon Trainer": "포켓몬 트레이너",
+    "Pichu": "피츄", "Incineroar": "어흥염", "Detective Pikachu": "명탐정 피카츄", "Shadow Mewtwo": "다크 뮤츠",
+
+    # 메트로이드 시리즈
+    "Samus": "사무스", "Zero Suit Samus": "제로 슈트 사무스", "Metroid": "메트로이드", "Samus Aran": "사무스 아란",
+    "E.M.M.I.": "E.M.M.I.", "Samus (Metroid Dread)": "사무스 (메트로이드 드레드)", "Dark Samus": "다크 사무스", "Ridley": "리들리",
+
+    # 기타 게임 시리즈
+    "Pit": "피트", "Palutena": "파르테나", "Dark Pit": "블랙피트", "Fox": "폭스", "Falco": "팔코", "Wolf": "울프",
+    "Duck Hunt": "덕헌트", "R.O.B.": "R.O.B.", "Mr. Game & Watch": "Mr. 게임&워치", "Ness": "네스", "Lucas": "류카",
+    "Captain Falcon": "캡틴 팔콘", "Olimar": "올리마", "Wii Fit Trainer": "Wii Fit 트레이너", "Shulk": "슈르크",
+    "Pac-Man": "팩맨", "Mega Man": "록맨", "Sonic": "소닉", "Ryu": "류", "Ken": "켄", "Cloud": "클라우드",
+    "Bayonetta": "베요네타", "Solaire of Astora": "태양의 전사 솔라", "Loot Goblin": "보물 고블린",
+    "Shovel Knight": "삽질 기사", "Plague Knight": "역병 기사", "Specter Knight": "스펙터 기사",
+    "King Knight": "킹 기사", "Gold Shovel Knight": "골드 삽질 기사", "Snake": "스네이크", "Simon": "시몬",
+    "Richter": "릭터", "King K. Rool": "킹크루루", "Piranha Plant": "뻐끔플라워", "Joker": "조커", "Hero": "용사",
+    "Banjo & Kazooie": "반조 & 카주이", "Terry": "테리", "Min Min": "미엔미엔", "Steve": "스티브", "Alex": "알렉스",
+    "Sephiroth": "세피로스", "Pyra": "호무라", "Mythra": "히카리", "Kazuya": "카즈야", "Sora": "소라",
+    "Magnamalo": "마가이마가도", "Palamute": "동반자 가루크", "Palico": "동반자 아이루", "Tsukino": "츠키노",
+    "Ena": "에나", "Razewing Ratha": "파멸의 날개 레우스", "Qbby": "큐비", "Noah": "노아", "Mio": "미오",
+    "Hammer Slam Bowser": "해머 슬램 쿠파", "Turbo Charge Donkey Kong": "터보 차지 동키콩", "Super Mario Cereal": "슈퍼 마리오 시리얼",
+    "Mega Man - Gold Edition": "록맨 (골드 에디션)", "R.O.B. - Famicom": "R.O.B. (패미컴)", "R.O.B. - NES": "R.O.B. (NES)",
+    "Cloud - Player 2": "클라우드 (2P)", "Bayonetta - Player 2": "베요네타 (2P)",
+    "One-Eyed Rathalos and Rider - Female": "리오레우스 & 라이더 (여)", "One-Eyed Rathalos and Rider - Male": "리오레우스 & 라이더 (남)",
+    "Rathian and Cheval": "리오레이아 & 슈발", "Qurupeco and Dan": "쿠루펫코 & 단", "Barioth and Ayuria": "벨리오로스 & 아유리아",
+    "Power-Up Band": "파워업 밴드", "Yuga Ohdo": "오도 유가", "Tatsuhisa “Luke” Kamijō": "카미조 타츠히사 “루크”", "Gakuto Sōgetsu": "소게츠 가쿠토",
+    "Romin Kirishima": "키리시마 로민", "Roa Kirishima": "키리시마 로아", "Nail Saionji": "사이온지 네일", "Asana Mutsuba": "무츠바 아사나",
+    "Pawapuro": "파워프로군", "Ikari": "이카리", "Hayakawa": "하야카와", "Ganda": "간다", "Daijobu": "다이죠부", "Yabe": "야베"
 }
 
 gameseries_translation = {
-    "Animal Crossing": "동물의 숲", "Sonic": "소닉", "Mii": "Mii", "Mario Sports Superstars": "마리오 스포츠 슈퍼스타즈",
-    "The Legend of Zelda": "젤다의 전설", "Splatoon": "스플래툰", "BoxBoy!": "BOXBOY!", "Yu-Gi-Oh!": "유희왕",
-    "Donkey Kong": "동키콩", "Final Fantasy": "파이널 판타지", "Kellogs": "켈로그", "Punch Out": "펀치 아웃!!",
-    "Megaman": "록맨", "Power Pros": "파워풀 프로야구", "Kirby": "별의 커비", "Fire Emblem": "파이어 엠블렘",
-    "Star Fox": "스타폭스", "Pokemon": "포켓몬", "Metroid": "메트로이드", "Kid Icarus": "키드 이카루스",
-    "Pikmin": "피크민", "Wii Fit": "Wii Fit", "Xenoblade Chronicles": "제노블레이드 크로니클스", "Pac-man": "팩맨",
-    "Street Fighter": "스트리트 파이터", "Bayonetta": "베요네타", "Chibi Robo": "작은 로보!", "Shovel Knight": "삽질 기사",
-    "Dark Souls": "다크 소울", "Diablo": "디아블로", "Skylanders": "스카이랜더스", "Monster Hunter": "몬스터 헌터",
-    "Castlevania": "캐슬바니아", "Metal Gear Solid": "메탈 기어 솔리드", "Dragon Quest": "드래곤 퀘스트",
-    "Banjo-Kazooie": "반조-카주이", "Fatal Fury": "아랑전설", "ARMS": "ARMS", "Minecraft": "마인크래프트",
-    "Kingdom Hearts": "킹덤 하츠", "Monster Hunter Rise": "몬스터 헌터 라이즈", "Monster Hunter Stories": "몬스터 헌터 스토리즈",
-    "Tekken": "철권", "Classic Nintendo": "클래식 닌텐도", "Earthbound": "마더", "F-Zero": "F-ZERO", "Persona": "페르소나",
-    "Super Mario": "슈퍼 마리오", "Yoshi's Woolly World": "요시 울리 월드", "Super Smash Bros.": "슈퍼 스매시브라더스"
+    "Super Mario": "슈퍼 마리오", "The Legend of Zelda": "젤다의 전설", "Super Smash Bros.": "슈퍼 스매시브라더스",
+    "Yoshi's Woolly World": "요시 울리 월드", "Animal Crossing": "동물의 숲", "Splatoon": "스플래툰",
+    "Kirby": "별의 커비", "Fire Emblem": "파이어 엠블렘", "Star Fox": "스타폭스", "Pokemon": "포켓몬",
+    "Metroid": "메트로이드", "Kid Icarus": "키드 이카루스", "Pikmin": "피크민", "Wii Fit": "Wii Fit",
+    "Xenoblade Chronicles": "제노블레이드 크로니클스", "Pac-Man": "팩맨", "Mega Man": "록맨", "Sonic the Hedgehog": "소닉 더 헤지혹",
+    "Street Fighter": "스트리트 파이터", "Final Fantasy": "파이널 판타지", "Bayonetta": "베요네타",
+    "Chibi-Robo!": "작은 로보!", "Shovel Knight": "삽질 기사", "Dark Souls": "다크 소울", "Diablo": "디아블로",
+    "Skylanders": "스카이랜더스", "Monster Hunter": "몬스터 헌터", "BoxBoy!": "BOXBOY!", "Castlevania": "캐슬바니아",
+    "Metal Gear Solid": "메탈 기어 솔리드", "Dragon Quest": "드래곤 퀘스트", "Banjo-Kazooie": "반조-카주이",
+    "Fatal Fury": "아랑전설", "ARMS": "ARMS", "Minecraft": "마인크래프트", "Kingdom Hearts": "킹덤 하츠",
+    "Monster Hunter Rise": "몬스터 헌터 라이즈", "Monster Hunter Stories": "몬스터 헌터 스토리즈",
+    "Mario Sports Superstars": "마리오 스포츠 슈퍼스타즈", "Power Pros": "파워 프로", "Yu-Gi-Oh!": "유희왕",
+    "Super Nintendo World": "슈퍼 닌텐도 월드", "Kellogs": "켈로그", "Xenoblade Chronicles 3": "제노블레이드 크로니클스 3",
+    "Tekken": "철권", "Persona": "페르소나", "Punch Out": "펀치 아웃!!", "Classic Nintendo": "클래식 닌텐도",
+    "F-Zero": "F-ZERO", "Earthbound": "MOTHER", "Megaman": "록맨"
 }
-# --- 전체 한글 번역 데이터 끝 ---
+# --- 한글 번역 데이터 추가 끝 ---
 
 class Amiibo:
     def __init__(self):
